8f08ad5dc4b4e40b5887f5690fb81a8a0bdf2d20
Jessica Jiang <jessicaj2947@gmail.com>
2023-03-27 22:23:11 +1300

    added a getcolumn function which gets the column they want to drop their token into from the player

--- ConnectFour.c
+++ ConnectFour.c
@@ -2,7 +2,7 @@
 want to drop their token into
 
 Author: Jessica Jiang
-Date Last Modified: 27/3/23*/
+Date Last Modified: */
 
 #include <stdio.h>
 #define width 7
@@ -52,10 +52,44 @@ void PrintBoard(char boardArray[height][width])
     }
 }
 
+/*Function that gets the column they want to drop their token into from the player*/
+int getColumn(void)
+{
+    int column, i;
+    int valid = 0;
+    i = 1;
+    printf("\nEnter Column Number: ");
+
+    // Ignore any invalid columns
+    scanf("%d", &column);
+
+    while (valid == 0 && i <= width)
+    {
+        if (i == column)
+        {
+            valid = 1;
+            return column;
+        }
+        else
+        {
+            i++;
+        }
+
+        if (i == width+1)
+        {
+            printf("Enter Column Number: ");
+            scanf("%d", &column);
+            i = 0;
+        }
+    }
+    return column;
+}
+
 int main(void)
 {
     char boardArray[height][width] = {0};
 
     PrintBoard(boardArray);
+    getColumn();
     return 0;
 }
